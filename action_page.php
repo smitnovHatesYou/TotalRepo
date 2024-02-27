df71ef1a6216696623eb16d7871edb519efd2808
Lafayette-Marquis <90972245+Lafayette-Marquis@users.noreply.github.com>
2024-02-27 09:36:07 -0500

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -3,12 +3,14 @@ php
 // Connect to SQLite database
 $db = new SQLite3('database.db');
 
-// Get data from the form
-$name = $_POST['name'];
+// Get form data
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
 $email = $_POST['email'];
+$dob = $_POST['dob'];
 
 // Insert data into the database
-$query = "INSERT INTO users (name, email) VALUES ('$name', '$email')";
+$query = "INSERT INTO users (first_name, last_name, email, dob) VALUES ('$first_name', '$last_name', '$email', '$dob')";
 $db->exec($query);
 
 // Close the database connection
